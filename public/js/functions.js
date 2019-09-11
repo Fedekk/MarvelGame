77b477f5bfb4bcbcd62d32e6a759f9b83ce84057
Fedek <f.shipwright@gmail.com>
2019-09-11 21:32:19 +0200

    aggiunta altra funzione

--- public/js/functions.js
+++ public/js/functions.js
@@ -6,20 +6,26 @@ document.addEventListener('DOMContentReady', domReady);
 
 
 function play(e, data, numero){
-    e.preventDefault();
-    let personaggio = getPersonaggioMarvel();
-    console.log(personaggio.id);
-    let el = document.getElementById('feedback');
-    let n = getEstrazione(100);
-    if(numero != n)
+    e.preventDefault(); //evito il normale comportamento del bottone
+    let el = document.getElementById('feedback'); //catturo l'elemento di feedback
+    let n = getEstrazione(10); //faccio estrarre un numero da 1 a 100
+    if(numero != n) //se corrsiponde a quello memorizzato su redis, ha vinto
         el.innerHTML = `Hai perso, mi spiace. E' uscito il numero ${ n }`;
-    else
+    else{
         el.innerHTML= `Hai vinto `;
+        if(data.includes("personaggi")){
+            let personaggio = getPersonaggioMarvel();
+            storeCharacter(personaggio);
+        }
+        else{
+            let comic = getComicMarvel();
+            storeComic(comic);
+        }
+    }
 }
 /** Template */
 // CRD operations character
 function storeCharacter(data){
-
 }
 function showCharacter(id){
 
@@ -44,7 +50,7 @@ function deleteComic(id){
 
 //  Estrazione numero da 1 a 100
 function getEstrazione(max){
-    estr = Math.floor((Math.random() * max) +1);
+    estr = Math.floor((Math.random() * max) + 1);
     return estr;
 }
 
@@ -62,7 +68,8 @@ function getPersonaggioMarvel(){
     .then(function(response){
         let elemRandom = getEstrazione(20)-1;
         let elemSelected = response.data.results[elemRandom];
-        let img = response.data.results[elemRandom].thumbnail.path;
+        let img;
+        if(img = response.data.results[elemRandom].thumbnail.path)
         if(!img.includes("image_not_available")){
             img += "/portrait_incredible.jpg";
         }
@@ -75,13 +82,40 @@ function getPersonaggioMarvel(){
             image: img
         }
     })
-    .catch(error => console.log("Si e' verificato un errore."));
+    .catch(error => console.log("Si e' verificato un errore: "+ error));
     while(personaggio == null);
     return personaggio;
 }
 
 // Prendo un fumetto random dalla marvel con titolo e immagine. Possibilmente anche descrizione
 function getComicMarvel(){
-
+    let apiKey = `22f5fe1303b0ffd673a3581119ea058b`;
+    let hash = `73aa13401993fe2b9cdc099524b39c6b`;
+    let ts = `1`;
+    let KEY = `ts=${ts}&apikey=${apiKey}&hash=${hash}`;
+    let url = `http://gateway.marvel.com/v1/public/comics?offset=${String(getEstrazione(74))}&${KEY}`;
+     fetch(url)
+    .then(response => {
+        if(response.ok) return response.json();
+    })
+    .then(function(response){
+        let elemRandom = getEstrazione(20)-1;
+        let elemSelected = response.data.results[elemRandom];
+        let img = response.data.results[elemRandom].thumbnail.path;
+        if(!img.includes("image_not_available")){
+            img += "/portrait_incredible.jpg";
+        }
+        else{
+            img+=".jpg";
+        }
+        comic = {
+            id: elemSelected.id,
+            name: elemSelected.title,
+            image: img
+        }
+    })
+    .catch(error => console.log("Si e' verificato un errore: " + error));
+    while(comic == null);
+    return comic;
 }
 
